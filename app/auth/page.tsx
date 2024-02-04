e8093ecee3bd079f1d796119a670e1fe28d10eb5
Pallav Goswami <pallav.goswami321@gmail.com>
2024-02-05 00:47:27 +0530

    updated page.tsx

---
+++ app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthenticationPage = () => {
+  return (
+    <div>
+      <h1>AuthenticationPage</h1>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
